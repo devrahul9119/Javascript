617b7b5e34dd11b782fd036ab36f50c81aca1ddb
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-11-28 07:39:49 +0000

    conversion operator

---
+++ basics/conversion_operator.js
@@ -0,0 +1,16 @@
+let num = 'Rahul'
+console.log(typeof(num))
+
+let valueInNumber = Number('55')
+console.log(typeof(valueInNumber))
+
+response = true
+console.log(Number(response))               // True =>1,False =>0
+
+result = 568
+console.log(String(result))
+
+load = 0                                  // 1=> True,0=>False
+console.log(Boolean(load))
+
+ 
