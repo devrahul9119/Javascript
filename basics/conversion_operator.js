fd87129363aa4b6732413d4c670a0588cf429dd8
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-11-28 07:49:08 +0000

    operator in js

--- basics/conversion_operator.js
+++ basics/conversion_operator.js
@@ -1,16 +1,24 @@
-let num = 'Rahul'
-console.log(typeof(num))
+// let num = 'Rahul'
+// console.log(typeof(num))
 
-let valueInNumber = Number('55')
-console.log(typeof(valueInNumber))
+// let valueInNumber = Number('55')
+// console.log(typeof(valueInNumber))
 
-response = true
-console.log(Number(response))               // True =>1,False =>0
+// response = true
+// console.log(Number(response))               // True =>1,False =>0
 
-result = 568
-console.log(String(result))
+// result = 568
+// console.log(String(result))
 
-load = 0                                  // 1=> True,0=>False
-console.log(Boolean(load))
+// load = 0                                  // 1=> True,0=>False
+// console.log(Boolean(load))
 
- 
+
+// let value = 5
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2-2)
