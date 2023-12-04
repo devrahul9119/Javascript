eb4df4f2ad85869f599642028d4eb929370f2535
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-04 11:37:38 +0000

    memory types in javascrript

---
+++ basics/memory.js
@@ -0,0 +1,16 @@
+//Stack Memory :- Used in all Primitive datatypes
+var name = ' Rahul'
+console.log(name)
+let anothername = 'Dev'
+name = 'Dev'
+console.log(name)
+
+
+
+// Heap Memory :- used in all non-Primitive datatype
+
+let user = {
+    'email':'devrahul9119@mail.com',
+    'phone':'9034609119'
+}
+console.log(user.email)
