9af8fdedb5bf2b825f218fde5e3cd0f099b03211
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-04 11:23:57 +0000

    data type

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,4 +2,14 @@
 
 /*  alert('This is not a good practice') // This runs on the server not on node js */
 
+// Symbol datatype
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+console.log(id == anotherId)
 
+
+// function
+const sums = function sum(a,b){
+    console.log(a+b)
+}
+sums(5,5)
