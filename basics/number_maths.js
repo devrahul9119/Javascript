be223b53bd386a6bd55a2c7f8e561d3e744110e7
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-09 08:36:10 +0000

    Math and numbers in javascript

---
+++ basics/number_maths.js
@@ -0,0 +1,20 @@
+const score = 98
+const marks = new Number(120)
+console.log(score)
+console.log(marks)
+
+console.log(marks.toString().length)
+console.log('The range of Number is from ' + Number.MIN_VALUE + ' to ' + Number.MAX_VALUE)
+
+const a = 100000000
+console.log(a.toLocaleString('en-IN'))
+
+
+// =================== MATHS ==================
+const num = 34.4535346
+console.log(Math)
+console.log(Math.round(9,8))
+console.log(Math.ceil(4.5))
+console.log(Math.floor(4.5))
+console.log(num.toFixed())
+console.log(num.toPrecision(4))
