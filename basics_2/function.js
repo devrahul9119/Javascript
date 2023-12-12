67a1e90417bf2f1faf6e79b7b81ae35291063e49
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-12 07:38:21 +0000

    arrow function in js

--- basics_2/function.js
+++ basics_2/function.js
@@ -1,20 +1,40 @@
-function greeting(name){
-    console.log(`Good Morning, ${name}`)
-}
-greeting('Rahul dev')
+// function greeting(name){
+//     console.log(`Good Morning, ${name}`)
+// }
+// greeting('Rahul dev')
 
-// greet('Rahul dev')   ----->>> It will return error ''greet can't access before initiliazation
-const greet = function(name){
-    console.log(`Good Morning, ${name}`)
-}
-greet('Rahul dev')
+// // greet('Rahul dev')   ----->>> It will return error ''greet can't access before initiliazation
+// const greet = function(name){
+//     console.log(`Good Morning, ${name}`)
+// }
+// greet('Rahul dev')
 
-// This keyword is used for current context
-const user = {
-    user_name : 'Rahul dev',
-    user_email : 'devrahul9119@gmail.com',
-    greet_user : function(){
-            console.log(`${this.user_name}(${this.user_email}), Welcome to the world of AI`)
-    }
-}
-user.greet_user()
+// // This keyword is used for current context
+// const user = {
+//     user_name : 'Rahul dev',
+//     user_email : 'devrahul9119@gmail.com',
+//     greet_user : function(){
+//             console.log(`${this.user_name}(${this.user_email}), Welcome to the world of AI`)
+//     }
+// }
+// user.greet_user()
+
+// console.log(typeof this)            // To check the type of this keyword 
+
+
+// function users(){
+//     const name = 'Rahul'            // We can't use this inside the function
+//     console.log(this.name)
+// }
+// users()
+
+// const users = function(){               // We can't use this inside the function
+//     const name = 'Rahul'
+//     console.log(this.name)
+// }
+// users()
+
+// Arrow functions
+const sum = (num1,num2) => num1+num2        // this is known as implicit return if we not use return keyword
+const sum1 = (num1,num2) => (num1+num2)     // this is known as implicit return if we not use return keyword
+const sum2 = (num1,num2) => ({user_name :'Rahul dev'})      // this is how to print the object by using the implicit return method
