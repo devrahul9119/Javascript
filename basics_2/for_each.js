cca522bc13f48ea6784328a277fa27979dad8c3b
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-12 13:47:01 +0000

    forEach looop in js

---
+++ basics_2/for_each.js
@@ -0,0 +1,45 @@
+a = ['Python','C++','Java','JavaScript','R','Stats']
+a.forEach(function (item){
+    console.log(item)               // Passing a function in for each loop
+})
+
+a.forEach((item) => {
+        console.log(item)           // Passing a arrow function to forEach loop
+})
+
+// Passing a external function to the forEach loop
+function print(item){
+    console.log(item)
+}
+
+a.forEach(print)
+
+// We can use index,item,whole array parameters in forEach loop
+a.forEach((item,index,arr) =>{
+    console.log(item,index,arr)
+})
+
+
+// multiple objects inside Array
+const student = [
+    {
+        studentName : 'Rahul dev',
+        studentId : 1007
+    },
+    {
+        studentName : 'Ram',
+        studentId : 1008
+    },
+    {
+        studentName :'Krishna',
+        studentId : 1009
+    },
+    {
+        studentName : 'Lord Rama',
+        studentId : 1010
+    },
+]
+
+student.forEach((item) =>{
+    console.log(item.studentName)
+})
