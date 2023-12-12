b46c4657946419973a7f72f836cc2b98f0bc4ea5
Rahul dev <133052585+devrahul9119@users.noreply.github.com>
2023-12-12 07:03:50 +0000

    added object inside funtion in the code

--- basics_2/function.js
+++ basics_2/function.js
@@ -9,3 +9,12 @@ const greet = function(name){
 }
 greet('Rahul dev')
 
+// This keyword is used for current context
+const user = {
+    user_name : 'Rahul dev',
+    user_email : 'devrahul9119@gmail.com',
+    greet_user : function(){
+            console.log(`${this.user_name}(${this.user_email}), Welcome to the world of AI`)
+    }
+}
+user.greet_user()
